cf8d1888878ef9919ebb8d3b208b480b59089252
Simon Pawar <simonpawar4@gmail.com>
2018-08-08 23:00:03 -0400

    API created

---
+++ index.js
@@ -0,0 +1,136 @@
+const fs = require('fs');
+const readline = require('readline');
+const {google} = require('googleapis');
+var http = require('http');
+
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const TOKEN_PATH = 'token.json';
+
+http.createServer(function(req, res){
+  res.write('Hello World');
+  res.end();
+}).listen(3000);
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Gmail API.
+  authorize(JSON.parse(content), findMessages);
+});
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0]);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client);
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+// function listLabels(auth) {
+//   const gmail = google.gmail({version: 'v1', auth});
+//   gmail.users.labels.list({
+//     userId: 'me',
+//   }, (err, res) => {
+//     if (err) return console.log('The API returned an error: ' + err);
+//     const labels = res.data.labels;
+//     if (labels.length) {
+//       console.log('Labels:');
+//       labels.forEach((label) => {
+//         console.log(`- ${label.name}`);
+//       });
+//     } else {
+//       console.log('Null');
+//     }
+//  });
+//  }
+
+
+//  function getMessage(auth) {
+//   var gmail = google.gmail({ auth: auth, version: 'v1' });
+
+//   gmail.users.messages.get({
+//     'userId': 'simonpawar5@gmail.com',
+//     'id': "rfc822msgid:1Tyr11ax2CSykuO-9bXpwA.0@notifications.google.com"
+//   }, function (err, result) {
+//     console.log(result);
+//   });
+// }
+
+
+function findMessages(auth) {
+  var gmail = google.gmail('v1');
+  gmail.users.messages.list({
+  auth: auth,
+  userId: 'simonpawar5@gmail.com',
+  maxResults: 10,
+  q:""
+}, function(err, response) {
+    console.log(response.data.messages[0].id);
+    console.log("++++++++++++++++++++++++++");
+
+
+
+    for(let i=0; i<10; i++) {
+      var gmail = google.gmail('v1');
+
+      gmail.users.messages.get({
+        auth: auth,
+        userId: 'me',
+        id: response.data.messages[i].id
+      }, function(err, response) {
+          console.log("====================================")
+          console.log(response);
+      });
+    }
+  });
+}
+
+
